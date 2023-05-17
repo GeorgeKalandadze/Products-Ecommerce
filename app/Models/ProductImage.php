ccfadfd164ce6d283493c5383b03ef24fe1626e2
George Kalandadze <kalandadzeg777@gmail.com>
2023-05-17 16:12:00 +0400

    feat:add realtions between models

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -5,19 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Product extends Model
+class ProductImage extends Model
 {
     use HasFactory;
 
-    public function subCategory():BelongsTo
+    public function product():BelongsTo
     {
         return $this->belongsTo(Product::class);
-    }
-
-    public function productImages(): HasMany
-    {
-        return $this->hasMany(ProductImage::class);
     }
 }
