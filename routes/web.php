7dfc2f2b5f911dbdea64df084bb16229ad05cef4
George Kalandadze <kalandadzeg777@gmail.com>
2023-05-17 22:39:13 +0400

    feat:add some changes in routing

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,17 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//
+//Route::get('/', function () {
+////    return Inertia::render('Welcome', [
+////        'canLogin' => Route::has('login'),
+////        'canRegister' => Route::has('register'),
+////        'laravelVersion' => Application::VERSION,
+////        'phpVersion' => PHP_VERSION,
+////    ]);
+//    return redirect()->route('home');
+//});
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
