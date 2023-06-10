858269455b3595480eec06c599badbb4087d6960
George Kalandadze <kalandadzeg777@gmail.com>
2023-06-10 20:25:56 +0400

    feat:improve navbar styles

--- resources/js/Components/ProductCard/ProductCard.jsx
+++ resources/js/Components/ProductCard/ProductCard.jsx
@@ -2,7 +2,7 @@ import ProductImage from '../../assets/laptop.jpg';
 import PrimaryButton from "@/Components/PrimaryButton";
 const ProductCard = () => {
     return(
-        <div className="p-4 transition duration-300 shadow-lg w-[350px]">
+        <div className="p-4 transition duration-300 shadow-lg w-[350px] rounded">
             <img src={ProductImage} className='w-full h-[20.63rem] cursor-pointer'/>
             <p className="text-[1.17rem] mt-4 opacity-90 ">MackBook Air 77</p>
             <div className="mt-4 flex justify-between">
--- resources/js/Layouts/Navbar/Navbar.jsx
+++ resources/js/Layouts/Navbar/Navbar.jsx
@@ -1,16 +1,28 @@
 import NavLogo from "@/Layouts/Navbar/NavbarComps/NavLogo";
 import BurgerMenu from "@/Layouts/Navbar/NavbarComps/BurgerMenu/BurgerMenu.jsx";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
 
 const Navbar = () => {
     return(
-        <div className="shadow-md p-6 px-[10px] flex justify-between md:px-[80px]">
-            <NavLogo/>
+        <div className="shadow-md p-6 px-2 flex justify-between md:px-[80px]">
+            <NavLogo />
+            <ul className="flex items-center justify-between gap-8">
+                <li>All Products</li>
+                <li>Categories</li>
+                <li>My Orders</li>
+            </ul>
             <div className="flex gap-4 items-center">
-                <ShoppingCartOutlinedIcon
-                    className="cursor-pointer bg-transparent"
-                />
-                <BurgerMenu/>
+                <SearchTwoToneIcon className="cursor-pointer"/>
+                <ShoppingCartOutlinedIcon className="cursor-pointer" />
+                <select
+                    className="appearance-none text-gray-700 leading-tight  bg-transparent border-none font-bold outline-0 cursor-pointer"
+                >
+                    <option value="en" >EN</option>
+                    <option value="fr">KA</option>
+                    {/* Add more language options as needed */}
+                </select>
+                {/*<BurgerMenu/>*/}
             </div>
         </div>
     )
