cf99ec05139a09eabfa91a4bc6e3607dc9f96171
George Kalandadze <kalandadzeg777@gmail.com>
2023-06-11 13:01:19 +0400

    feat:make seeder for categories and subcategories

--- database/migrations/2023_05_13_190010_create_products_table.php
+++ database/migrations/2023_05_13_190010_create_products_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedInteger('subcategory_id');
             $table->string('slug');
             $table->boolean('is_new');
             $table->integer('quantity');
