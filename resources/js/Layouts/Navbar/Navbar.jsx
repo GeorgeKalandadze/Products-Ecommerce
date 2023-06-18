e4c4b3571c24bde7f4f313825a0c95e75d66f3d3
George Kalandadze <kalandadzeg777@gmail.com>
2023-06-19 02:24:17 +0400

    feat:make nested dropdown for categories and subcategories

--- resources/js/Layouts/Navbar/Navbar.jsx
+++ resources/js/Layouts/Navbar/Navbar.jsx
@@ -1,31 +1,94 @@
+import React, { useEffect, useState } from 'react';
 import NavLogo from "@/Layouts/Navbar/NavbarComps/NavLogo";
-import BurgerMenu from "@/Layouts/Navbar/NavbarComps/BurgerMenu/BurgerMenu.jsx";
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import {Button, Menu, MenuItem} from '@mui/material';
+import axios from "axios";
+import { Dropdown, DropdownMenuItem, DropdownNestedMenuItem } from "./NavbarComps/Dropdown/Dropdown.jsx";
 
 const Navbar = () => {
-    return(
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [categories, setCategories] = useState([]);
+    const [subcategories, setSubcategories] = useState([]);
+    const [hoveredCategory, setHoveredCategory] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleCategoryHover = (categoryId) => {
+        const hoveredCategory = categories.find(category => category.id === categoryId);
+        if (hoveredCategory) {
+            setHoveredCategory(hoveredCategory);
+            setSubcategories(hoveredCategory.sub_categories);
+        }
+    };
+
+    useEffect(() => {
+        axios
+            .get(`${window.location.protocol}//${window.location.host}/api/categories`)
+            .then((res) => {
+                setCategories(res.data);
+            });
+    }, []);
+
+    const handleMouseLeave = () => {
+        setHoveredCategory(null);
+        setSubcategories([]);
+    };
+
+    console.log(categories)
+
+    return (
         <div className="shadow-md p-6 px-2 flex justify-between md:px-[80px]">
             <NavLogo />
             <ul className="flex items-center justify-between gap-8">
-                <li>All Products</li>
-                <li>Categories</li>
-                <li>My Orders</li>
+                <li className="cursor-pointer">All Products</li>
+                <li
+                    className="cursor-pointer"
+                    onMouseEnter={() => handleCategoryHover('categories')}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    <div onClick={handleClick}>
+                        <Dropdown
+                            trigger={<Button>Categories</Button>}
+                            menu={[
+                                categories.map((category) => (
+                                    <DropdownNestedMenuItem
+                                        label={category.name}
+
+                                        menu={
+                                        [
+                                            category.sub_categories.map((subcat) => (
+                                                <DropdownMenuItem
+                                                    onClick={() => {
+                                                        console.log("clicked");
+                                                    }}
+                                                >
+                                                    {subcat.name}
+                                                </DropdownMenuItem>
+                                            ))
+
+                                        ]}
+                                    />
+                                ))
+                            ]}
+                        />
+                    </div>
+
+
+                </li>
+                <li className="cursor-pointer">My Orders</li>
             </ul>
             <div className="flex gap-4 items-center">
-                <SearchTwoToneIcon className="cursor-pointer"/>
                 <ShoppingCartOutlinedIcon className="cursor-pointer" />
-                <select
-                    className="appearance-none text-gray-700 leading-tight  bg-transparent border-none font-bold outline-0 cursor-pointer"
-                >
-                    <option value="en" >EN</option>
-                    <option value="fr">KA</option>
-                    {/* Add more language options as needed */}
-                </select>
-                {/*<BurgerMenu/>*/}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
