5dab527a833bc63f92eec3cc19c153190a0ba9e2
George Kalandadze <kalandadzeg777@gmail.com>
2023-06-20 01:35:28 +0400

    feat:make factories and seeders for products and product images

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -14,7 +15,9 @@ class ProductsController extends Controller
     public function index()
     {
 
-        return Inertia::render('Product/Products');
+        return Inertia::render('Product/Products',[
+           
+        ]);
     }
 
     /**
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\SubCategory;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'slug' => $this->faker->slug,
+            'is_new' => $this->faker->boolean,
+            'quantity' => $this->faker->numberBetween(0, 100),
+            'description' => $this->faker->paragraph,
+            'cart_image' => $this->faker->imageUrl(), // Generate a random image URL
+            'price' => $this->faker->randomFloat(2, 0, 1000),
+            'subcategory_id' => SubCategory::inRandomOrder()->first()->id,
+        ];
+    }
+}
