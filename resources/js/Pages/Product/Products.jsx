db04f2c0ee0232886a2b77aa4651217b0bae5231
George Kalandadze <kalandadzeg777@gmail.com>
2023-06-20 02:48:31 +0400

    feat:use products in front

--- resources/js/Pages/Product/Products.jsx
+++ resources/js/Pages/Product/Products.jsx
@@ -4,14 +4,13 @@ import ProductCard from "@/Components/ProductCard/ProductCard.jsx";
 
 
 export default function Products(props){
-    console.log(JSON.parse(props.products))
+    const products = JSON.parse(props.products)
     return (
         <ProductsLayout>
-            <div className="flex justify-between">
-                <ProductCard />
-                <ProductCard/>
-                <ProductCard/>
-
+            <div className="flex justify-between flex-wrap gap-6">
+                {products.map((product) => (
+                    <ProductCard key={product.id} props={product}/>
+                ))}
             </div>
         </ProductsLayout>
     )
