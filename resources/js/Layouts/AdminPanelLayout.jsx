bb39571b76dd875adbc430f0fa5ad57cdffca0c6
George Kalandadze <kalandadzeg777@gmail.com>
2023-06-24 19:00:00 +0400

    chore:make better styles for sidebar and add blur when is open

--- resources/js/Layouts/AdminPanelLayout.jsx
+++ resources/js/Layouts/AdminPanelLayout.jsx
@@ -1,47 +1,48 @@
-import Sidebar from "@/Layouts/SideBar/Sidebar";
-import {useState} from "react";
-import Box from "@mui/material/Box";
-import MenuIcon from "@mui/icons-material/Menu.js";
+import React, { useState } from 'react';
+import Sidebar from '@/Layouts/SideBar/Sidebar';
+import Box from '@mui/material/Box';
+import MenuIcon from '@mui/icons-material/Menu.js';
 import PersonAvatar from '../assets/person.jpg';
 import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
-import {Modal} from "@mui/material";
+import { Modal } from '@mui/material';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
 
-
-
-const AdminPanelLayout = ({children}) => {
+const AdminPanelLayout = ({ children }) => {
     const [open, setOpen] = useState(false);
-    const [openModal, setOpenModal] = useState(false)
+    const [openModal, setOpenModal] = useState(false);
+
     const handleDrawerOpen = () => {
-        setOpen(isopen => !isopen);
+        setOpen((isOpen) => !isOpen);
     };
 
     const style = {
-        position: 'absolute' ,
+        position: 'absolute',
         top: '10%',
         right: '2%',
-        outline:"none",
+        outline: 'none',
         width: 200,
         bgcolor: 'background.paper',
         border: '0',
         boxShadow: 24,
-        borderRadius:"6px",
+        borderRadius: '6px',
         p: 2,
-        display:"flex",
-        flexDirection:"column",
-        gap:"15px"
-
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '15px',
     };
-    return(
+
+    return (
         <>
-            <Box sx={{ display: 'flex', flexDirection:'column' }}>
-                <Sidebar isOpen={open} />
-                <nav  className="w-full p-4 bg-gray-100 flex justify-between items-center">
-                    <MenuIcon sx={{color:"black", cursor:"pointer"}} onClick={handleDrawerOpen}/>
-                    <div className="flex gap-4 items-center relative">
-                        <img src={PersonAvatar} className="h-10 w-10 rounded-full object-cover"/>
-                        <p className="cursor-pointer" onClick={() => setOpenModal(isOpen => !isOpen)}>David <ArrowDropDownOutlinedIcon/></p>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Sidebar isOpen={open} setOpen={setOpen}/>
+                <nav className="w-full p-4 bg-gray-100 flex justify-between items-center">
+                    <MenuIcon sx={{ color: 'black', cursor: 'pointer' }} onClick={handleDrawerOpen} />
+                    <div className={`flex gap-4 items-center relative ${open ? 'blur' : ''}`}>
+                        <img src={PersonAvatar} className="h-10 w-10 rounded-full object-cover" />
+                        <p className="cursor-pointer" onClick={() => setOpenModal((isOpen) => !isOpen)}>
+                            David <ArrowDropDownOutlinedIcon />
+                        </p>
                         <Modal
                             aria-labelledby="modal-modal-title"
                             aria-describedby="modal-modal-description"
@@ -49,18 +50,31 @@ const AdminPanelLayout = ({children}) => {
                             onClose={() => setOpenModal(false)}
                         >
                             <Box sx={style}>
-                                <p className="text-[15px] flex gap-3"><PermIdentityOutlinedIcon sx={{color:"#8691f8"}}/> Profile</p>
-                                <p className="text-[15px] flex gap-3"><LogoutOutlinedIcon sx={{color:"#8691f8"}}/> Logout</p>
+                                <p className="text-[15px] flex gap-3">
+                                    <PermIdentityOutlinedIcon sx={{ color: '#8691f8' }} /> Profile
+                                </p>
+                                <p className="text-[15px] flex gap-3">
+                                    <LogoutOutlinedIcon sx={{ color: '#8691f8' }} /> Logout
+                                </p>
                             </Box>
                         </Modal>
                     </div>
                 </nav>
-                {children}
+                <div className={`content ${open ? 'blur' : ''}`}>
+                    {children}
+                </div>
             </Box>
 
+            <style jsx>{`
+        .blur {
+          filter: blur(4px);
+        }
+        .content {
+          transition: filter 0.3s ease;
+        }
+      `}</style>
         </>
+    );
+};
 
-    )
-}
-
-export default AdminPanelLayout
+export default AdminPanelLayout;
--- resources/js/Layouts/SideBar/Sidebar.jsx
+++ resources/js/Layouts/SideBar/Sidebar.jsx
@@ -9,17 +9,14 @@ import ListItemText from '@mui/material/ListItemText';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
-
-
-
-const PersistentDrawerLeft = ({isOpen}) => {
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+const PersistentDrawerLeft = ({ isOpen,setOpen }) => {
     const theme = useTheme();
-
 
     return (
         <Drawer
             sx={{
-                backgroundColor:"#423dce",
+                backgroundColor: '#423dce',
                 width: isOpen ? 240 : 0,
                 flexShrink: 0,
                 '& .MuiDrawer-paper': {
@@ -31,66 +28,54 @@ const PersistentDrawerLeft = ({isOpen}) => {
             anchor="left"
             open={isOpen}
         >
-
-            <List sx={{
-                backgroundColor:"#423dce",
-                height:"100vh"
-            }}>
-                    <ListItem disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                  <HomeOutlinedIcon sx={{color:"white"}}/>
-                            </ListItemIcon>
-                            <ListItemText primary={"Dashboard"} sx={{
-                                color:"white",
-                            }}/>
-                        </ListItemButton>
-                    </ListItem>
+            <List sx={{ backgroundColor: '#423dce', height: '100vh' }}>
+                <ListItemButton>
+                    <ListItemIcon onClick={() => setOpen(false)}>
+                        <CloseOutlinedIcon sx={{ color: 'white' }} />
+                    </ListItemIcon>
+                </ListItemButton>
                 <ListItem disablePadding>
                     <ListItemButton>
                         <ListItemIcon>
-                            <MenuIcon sx={{color:"white"}}/>
+                            <HomeOutlinedIcon sx={{ color: 'white' }} />
                         </ListItemIcon>
-                        <ListItemText primary={"Products"} sx={{
-                            color:"white",
-                        }}/>
-                    </ListItemButton>
-                </ListItem>
-
-                <ListItem disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <MenuIcon sx={{color:"white"}}/>
-                        </ListItemIcon>
-                        <ListItemText primary={"Orders"} sx={{
-                            color:"white",
-                        }}/>
+                        <ListItemText primary={'Dashboard'} sx={{ color: 'white' }} />
                     </ListItemButton>
                 </ListItem>
                 <ListItem disablePadding>
                     <ListItemButton>
                         <ListItemIcon>
-                            <GroupOutlinedIcon sx={{color:"white"}}/>
+                            <MenuIcon sx={{ color: 'white' }} />
                         </ListItemIcon>
-                        <ListItemText primary={"Users"} sx={{
-                            color:"white",
-                        }}/>
+                        <ListItemText primary={'Products'} sx={{ color: 'white' }} />
                     </ListItemButton>
                 </ListItem>
                 <ListItem disablePadding>
                     <ListItemButton>
                         <ListItemIcon>
-                            <GroupOutlinedIcon sx={{color:"white"}}/>
+                            <MenuIcon sx={{ color: 'white' }} />
                         </ListItemIcon>
-                        <ListItemText primary={"Customers"} sx={{
-                            color:"white",
-                        }}/>
+                        <ListItemText primary={'Orders'} sx={{ color: 'white' }} />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <GroupOutlinedIcon sx={{ color: 'white' }} />
+                        </ListItemIcon>
+                        <ListItemText primary={'Users'} sx={{ color: 'white' }} />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                        <ListItemIcon>
+                            <GroupOutlinedIcon sx={{ color: 'white' }} />
+                        </ListItemIcon>
+                        <ListItemText primary={'Customers'} sx={{ color: 'white' }} />
                     </ListItemButton>
                 </ListItem>
             </List>
         </Drawer>
-
-
     );
 };
 
