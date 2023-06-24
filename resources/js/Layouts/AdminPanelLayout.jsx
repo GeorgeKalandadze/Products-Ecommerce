914e406831e709fb42c61ae80a82e583de2aae40
George Kalandadze <kalandadzeg777@gmail.com>
2023-06-24 17:37:56 +0400

    feat:make navbar and small modal for adminpanel

--- resources/js/Layouts/AdminPanelLayout.jsx
+++ resources/js/Layouts/AdminPanelLayout.jsx
@@ -1,14 +1,63 @@
 import Sidebar from "@/Layouts/SideBar/Sidebar";
 import {useState} from "react";
+import Box from "@mui/material/Box";
+import MenuIcon from "@mui/icons-material/Menu.js";
+import PersonAvatar from '../assets/person.jpg';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import {Modal} from "@mui/material";
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
 
-const AdminPanelLayout = () => {
 
+
+const AdminPanelLayout = ({children}) => {
+    const [open, setOpen] = useState(false);
+    const [openModal, setOpenModal] = useState(false)
+    const handleDrawerOpen = () => {
+        setOpen(isopen => !isopen);
+    };
+
+    const style = {
+        position: 'absolute' ,
+        top: '10%',
+        right: '2%',
+        outline:"none",
+        width: 200,
+        bgcolor: 'background.paper',
+        border: '0',
+        boxShadow: 24,
+        borderRadius:"6px",
+        p: 2,
+        display:"flex",
+        flexDirection:"column",
+        gap:"15px"
+
+    };
     return(
         <>
-            <div >
-                open sidebar
-            </div>
-            <Sidebar isOpen={open}/>
+            <Box sx={{ display: 'flex', flexDirection:'column' }}>
+                <Sidebar isOpen={open} />
+                <nav  className="w-full p-4 bg-gray-100 flex justify-between items-center">
+                    <MenuIcon sx={{color:"black", cursor:"pointer"}} onClick={handleDrawerOpen}/>
+                    <div className="flex gap-4 items-center relative">
+                        <img src={PersonAvatar} className="h-10 w-10 rounded-full object-cover"/>
+                        <p className="cursor-pointer" onClick={() => setOpenModal(isOpen => !isOpen)}>David <ArrowDropDownOutlinedIcon/></p>
+                        <Modal
+                            aria-labelledby="modal-modal-title"
+                            aria-describedby="modal-modal-description"
+                            open={openModal}
+                            onClose={() => setOpenModal(false)}
+                        >
+                            <Box sx={style}>
+                                <p className="text-[15px] flex gap-3"><PermIdentityOutlinedIcon sx={{color:"#8691f8"}}/> Profile</p>
+                                <p className="text-[15px] flex gap-3"><LogoutOutlinedIcon sx={{color:"#8691f8"}}/> Logout</p>
+                            </Box>
+                        </Modal>
+                    </div>
+                </nav>
+                {children}
+            </Box>
+
         </>
 
     )
--- resources/js/Pages/Admin/AdminPanel.jsx
+++ resources/js/Pages/Admin/AdminPanel.jsx
@@ -1,21 +1,10 @@
-import Sidebar from "@/Layouts/SideBar/Sidebar";
-import {useState} from "react";
-import Box from '@mui/material/Box';
-import MenuIcon from '@mui/icons-material/Menu';
-const AdminPanel = () => {
-    const [open, setOpen] = useState(false);
+import AdminPanelLayout from "@/Layouts/AdminPanelLayout.jsx";
 
-    const handleDrawerOpen = () => {
-        setOpen(isopen => !isopen);
-    };
+const AdminPanel = () => {
     return(
-        <Box sx={{ display: 'flex' }}>
-            <Sidebar isOpen={open} />
-            <div onClick={handleDrawerOpen} className="w-full p-4 bg-red-500">
-                <MenuIcon sx={{color:"white", cursor:"pointer"}}/>
-            </div>
-            <h1></h1>
-        </Box>
+       <AdminPanelLayout>
+           <h1>some</h1>
+       </AdminPanelLayout>
     )
 }
 
