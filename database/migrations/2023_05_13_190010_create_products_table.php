0f9803521976465b61c3739befd82243e4172d86
George Kalandadze <kalandadzeg777@gmail.com>
2023-07-16 23:38:41 +0400

    feat:make index on product nme

--- database/migrations/2023_05_13_190010_create_products_table.php
+++ database/migrations/2023_05_13_190010_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->index();
             $table->string('slug');
             $table->boolean('is_new');
             $table->integer('quantity');
