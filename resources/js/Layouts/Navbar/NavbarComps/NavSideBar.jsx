8f605078c9ebff58f4235ff8fb040f085a981c59
George Kalandadze <kalandadzeg777@gmail.com>
2023-07-20 17:04:54 +0400

    feat:make landing page for the admin from the products page to the admin panel

--- resources/js/Layouts/Navbar/NavbarComps/NavSideBar.jsx
+++ resources/js/Layouts/Navbar/NavbarComps/NavSideBar.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import MenuIcon from '@mui/icons-material/Menu';
 import LogoutIcon from '@mui/icons-material/Logout';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import {Link, useForm} from '@inertiajs/react';
+import {Link, useForm, usePage} from '@inertiajs/react';
 import {useGlobalContext} from "@/Context/Context.jsx";
 
 const PersistentDrawerLeft = () => {
     const {isOpenSidebar, setOpenSidebar} = useGlobalContext();
+    const user = usePage().props.auth.user;
     const { post } = useForm();
+
+    useEffect(() => {
+        setOpenSidebar(false)
+    },[usePage().url])
 
     return (
         <Drawer
@@ -36,6 +42,18 @@ const PersistentDrawerLeft = () => {
                         <CloseOutlinedIcon sx={{ color: 'white' }} />
                     </ListItemIcon>
                 </ListItemButton>
+                {user.is_admin ? (
+                    <Link href={route('adminPanel.adminPanel')}>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <AdminPanelSettingsIcon sx={{ color: 'white' }} />
+                                </ListItemIcon>
+                                <ListItemText primary={'Adminpanel'} sx={{ color: 'white' }} />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+                ):(<> </>)}
                 <Link href={route('orders')}>
                     <ListItem disablePadding >
                         <ListItemButton>
@@ -46,6 +64,7 @@ const PersistentDrawerLeft = () => {
                         </ListItemButton>
                     </ListItem>
                 </Link>
+
                 <Link onClick={() => post(route('logout'))} >
                     <ListItem disablePadding>
                         <ListItemButton>
@@ -56,8 +75,6 @@ const PersistentDrawerLeft = () => {
                         </ListItemButton>
                     </ListItem>
                 </Link>
-
-
             </List>
         </Drawer>
     );
